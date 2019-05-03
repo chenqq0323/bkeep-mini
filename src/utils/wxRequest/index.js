02a5f6e0daa6d7a0325f57e9a0a80f3ee7bebde3
chenqq0323 <847946090@qq.com>
2019-05-04 07:48:23 +0800

    小程序前端代码

---
+++ src/utils/wxRequest/index.js
@@ -0,0 +1,16 @@
+import Base from './base'
+import Host from '@/utils/host'
+const Get = async (url, params, options = {}) => await Base.doRequest(`${Host.url}/${url}`, 'GET', params, options)
+const Put = async (url, params, options = {}) => await Base.doRequest(`${Host.url}/${url}`, 'PUT', params, options)
+const Post = async (url, params, options = {}) => await Base.doRequest(`${Host.url}/${url}`, 'Post', params, options)
+const Destroy = async (url, params, options = {}) => await Base.doRequest(`${Host.url}/${url}`, 'DELETE', params, options)
+const Upload = async (filePath, params = {}) => await Base.wxUpload(`${Host.url}/upload`, filePath, params)
+const GetOpenId = async () => await Base.getOpenId()
+export default {
+  Get,
+  Put,
+  Post,
+  Destroy,
+  Upload,
+  GetOpenId
+}
